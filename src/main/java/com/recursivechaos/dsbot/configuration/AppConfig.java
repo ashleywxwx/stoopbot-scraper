f98ffa468760d978effecf6f47ee8d9ad6e858bb
AndrewBell <andrew@recursivechaos.com>
2015-07-12 17:25:33 -0500

    Properticized, and dockerized.

---
+++ src/main/java/com/recursivechaos/dsbot/configuration/AppConfig.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Andrew Bell 7/12/2015
+ * www.recursivechaos.com
+ * andrew@recursivechaos.com
+ * Licensed under MIT License 2015. See license.txt for details.
+ */
+
+package com.recursivechaos.dsbot.configuration;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "db.mongo")
+public class AppConfig {
+
+    private String url;
+
+    private String dbname;
+
+    @Bean
+    public Mongo mongo() throws Exception {
+        return new MongoClient(url);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), dbname);
+    }
+
+}
