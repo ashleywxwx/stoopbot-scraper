4f9ad5a6f4a6af1046a15d109f3a1dc3c0b56ef6
AndrewBell <andrew@recursivechaos.com>
2015-07-20 20:56:36 -0500

    Fixed my imports

--- src/test/java/com/recursivechaos/stoopbot/StoopbotScraperApplicationTest.java
+++ src/test/java/com/recursivechaos/stoopbot/StoopbotScraperApplicationTest.java
@@ -2,17 +2,17 @@ package com.recursivechaos.stoopbot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = StoopbotScraperApplication.class)
 @WebAppConfiguration
 public class StoopbotScraperApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
